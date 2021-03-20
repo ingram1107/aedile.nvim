0b92c47fe4df60dfade19a11f378cd905ed3349a
Ingram Ham <hamwenhong1@gmail.com>
2021-03-20 13:50:10 +0800

    license source code

--- lua/aedile.lua
+++ lua/aedile.lua
@@ -1,5 +1,20 @@
--- aedile.lua
--- a plugin that open REPL in a split window according to current buffer filetype
+--[[
+   aedile.lua   Nvim plugin that open REPL in a split window according to buf ft
+   Copyright (C) 2021  Little Clover
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--]]
 if vim.version().minor < 5 then
   vim.api.nvim_err_write("Neovim version < 0.5.0: deactivate all Aedile's functions")
   return
