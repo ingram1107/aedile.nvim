bbef9cc1c9c63a6ff4c1474005256bc1151a1eff
Little Clover <littleclover909@gmail.com>
2021-12-01 23:47:17 +0800

    feat: copy the terminal_job_id to the system clipboard
    instead of plain echoing the value of it

--- lua/aedile.lua
+++ lua/aedile.lua
@@ -88,8 +88,8 @@ function M.toggle_repl()
     term_win_id = vim.api.nvim_get_current_win()
     term_buf_id = vim.api.nvim_win_get_buf(term_win_id)
     term_job_id = vim.api.nvim_buf_get_var(term_buf_id, 'terminal_job_id')
+    vim.fn.setreg('+', term_job_id)
     mapping_update()
-    vim.api.nvim_echo({{"terminal jobid: "..term_job_id}}, false, {})
     toggle = true
   end
 
